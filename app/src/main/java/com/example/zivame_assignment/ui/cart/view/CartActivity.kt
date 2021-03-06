fd103bdf01419029264791d366affbade96dc989
Sudhan <sudhanram95@gmail.com>
2021-03-07 00:52:40 +0530

    View layer of cart module

--- app/src/main/java/com/example/zivame_assignment/ui/cart/view/CartActivity.kt
+++ app/src/main/java/com/example/zivame_assignment/ui/cart/view/CartActivity.kt
@@ -1,12 +1,48 @@
 package com.example.zivame_assignment.ui.cart.view
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.zivame_assignment.R
+import com.example.zivame_assignment.application.viewmodel.ViewModelFactory
+import com.example.zivame_assignment.ui.cart.viewmodel.CartViewModel
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_cart.*
+import javax.inject.Inject
 
 class CartActivity : DaggerAppCompatActivity() {
+
+    @Inject lateinit var viewModelFactory: ViewModelFactory
+    lateinit var cartViewModel: CartViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+
+        cartViewModel = ViewModelProvider(this, viewModelFactory).get(CartViewModel::class.java)
+        cartViewModel.fetchAllItemsInCart()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        cartViewModel.getCartList().observe(this, Observer {
+            val cartAdapter = CartAdapter(this, it.toMutableList())
+            rvCart.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+            rvCart.adapter = cartAdapter
+        })
+
+        cartViewModel.getResult().observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 }
--- app/src/main/java/com/example/zivame_assignment/ui/cart/view/CartAdapter.kt
+++ app/src/main/java/com/example/zivame_assignment/ui/cart/view/CartAdapter.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -14,7 +12,7 @@ import com.example.zivame_assignment.R
 import com.example.zivame_assignment.database.CartEntity
 
 class CartAdapter(val context: Context,
-                  val cartList: ArrayList<CartEntity>) : RecyclerView.Adapter<CartAdapter.Companion.MyViewHolder>() {
+                  val cartList: MutableList<CartEntity>) : RecyclerView.Adapter<CartAdapter.Companion.MyViewHolder>() {
 
     companion object {
         class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
