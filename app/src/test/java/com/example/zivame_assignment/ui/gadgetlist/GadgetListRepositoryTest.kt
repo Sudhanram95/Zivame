81db29d75a681ea59cbad72184a2c6233afccaee
Sudhan <sudhanram95@gmail.com>
2021-03-08 03:45:01 +0530

    Gadget list repository test coverage

--- app/src/test/java/com/example/zivame_assignment/ui/gadgetlist/GadgetListRepositoryTest.kt
+++ app/src/test/java/com/example/zivame_assignment/ui/gadgetlist/GadgetListRepositoryTest.kt
@@ -1,13 +1,18 @@
 package com.example.zivame_assignment.ui.gadgetlist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.example.zivame_assignment.BaseTest
 import com.example.zivame_assignment.database.CartDao
+import com.example.zivame_assignment.database.CartEntity
+import com.example.zivame_assignment.database.DatabaseCallback
 import com.example.zivame_assignment.database.ZivameDatabase
 import com.example.zivame_assignment.network.NetworkCallback
 import com.example.zivame_assignment.ui.gadgetlist.model.GadgetListResponse
 import com.example.zivame_assignment.ui.gadgetlist.repository.GadgetListApiService
 import com.example.zivame_assignment.ui.gadgetlist.repository.GadgetListRepository
+import com.google.common.util.concurrent.MoreExecutors
 import io.reactivex.Single
 import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertNotNull
@@ -18,6 +23,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.TimeUnit
@@ -30,15 +36,18 @@ class GadgetListRepositoryTest : BaseTest() {
 
     @Mock lateinit var apiService: GadgetListApiService
     @Mock lateinit var cartDao: CartDao
-    @Mock lateinit var executorService: ExecutorService
+    @Mock lateinit var cartEntity: CartEntity
+    @Mock lateinit var databaseCallback: DatabaseCallback
     @Mock lateinit var response: GadgetListResponse
     @Mock lateinit var networkCallback: NetworkCallback
     @Mock lateinit var ex: Exception
+    @Mock lateinit var observerBadgeCount: Observer<Int>
 
     lateinit var repository: GadgetListRepository
 
     @Before
     fun setUp() {
+        val executorService = MoreExecutors.newDirectExecutorService()
         repository = GadgetListRepository(apiService, executorService, cartDao)
     }
 
@@ -71,5 +80,34 @@ class GadgetListRepositoryTest : BaseTest() {
         val inOrder = Mockito.inOrder(networkCallback)
         inOrder.verify(networkCallback, Mockito.times(1)).onError(ex)
         Mockito.verify(networkCallback, Mockito.never()).onSuccess(response)
+    }
+
+    @Test
+    fun testAddGadgetToCart() {
+        `when`(cartDao.addItemToCart(cartEntity)).thenReturn(2.toLong())
+        repository.addGadgetToCart(cartEntity, databaseCallback)
+
+        val inOrder = Mockito.inOrder(databaseCallback)
+        inOrder.verify(databaseCallback, Mockito.times(1)).onSuccess(2.toLong())
+        Mockito.verify(databaseCallback, Mockito.never()).onFailure()
+    }
+
+    @Test
+    fun testAddGadgetToCartFailure() {
+        `when`(cartDao.addItemToCart(cartEntity)).thenReturn(-1.toLong())
+        repository.addGadgetToCart(cartEntity, databaseCallback)
+
+        val inOrder = Mockito.inOrder(databaseCallback)
+        inOrder.verify(databaseCallback, Mockito.times(1)).onFailure()
+        Mockito.verify(databaseCallback, Mockito.never()).onSuccess(-1.toLong())
+    }
+
+    @Test
+    fun testGetBadgeCount() {
+        val badgeCountLiveData = MutableLiveData<Int>()
+        badgeCountLiveData.value = 10
+        `when`(cartDao.getItemCount()).thenReturn(badgeCountLiveData)
+        assertEquals(badgeCountLiveData, repository.getBadgeCountFromDb())
+        assertEquals(10, repository.getBadgeCountFromDb()?.value)
     }
 }
